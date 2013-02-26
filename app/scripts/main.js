5d1f4c78408e04f94c979d9b80b7b13ee24639ac
Jose Jesus Perez Aguinaga <jj.perez.aguinaga@gmail.com>
2013-02-26 14:25:54 -0600

    Updated files in order to use the new audio video image setup

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,14 @@
-var f = new Firebase('https://jjperezaguinaga.firebaseio.com/photo_title');
-f.on('value', function(s) {
-  document.getElementById('image').src = s.val();
+var f = new Firebase('https://jjperezaguinaga.firebaseio.com/');
+f.on('child_changed', function(s) {
+	var elements = ["audio", "video", "image"];
+
+	for (var i = 0, len = elements.length; i < len; i ++) {
+		document.getElementById(elements[i]).style.visibility = "hidden";		
+	}
+
+	var typeOfElement = s.name();
+  	var ele = document.getElementById(typeOfElement);
+  	ele.src = s.val();
+  	ele.style.visibility = "visible";
+  	console.log(ele);
 });
