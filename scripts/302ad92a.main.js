3c4e8b38f12d98e21699a71c582c9ff674592569
Jose Jesus Perez Aguinaga <jj.perez.aguinaga@gmail.com>
2013-02-27 13:57:34 -0600

    Update git pages

---
+++ scripts/302ad92a.main.js
@@ -0,0 +1,25 @@
+var f = new Firebase('https://jjperezaguinaga.firebaseio.com/');
+
+var latestFeed = f.child('latest');
+latestFeed.on('value', function(s) {
+	var dataType = s.val() || "image";
+	var dataFeed = f.child(dataType);
+	dataFeed.on('value', function(r) {
+		var ele = document.getElementById(dataType)
+		ele.src = r.val();
+		ele.style.visibility = "visible";
+	})
+});
+
+f.on('child_changed', function(s) {
+	var elements = ["audio", "video", "image"];
+
+	for (var i = 0, len = elements.length; i < len; i ++) {
+		document.getElementById(elements[i]).style.visibility = "hidden";		
+	}
+
+	var typeOfElement = s.name();
+  	var ele = document.getElementById(typeOfElement);
+  	ele.src = s.val();
+  	ele.style.visibility = "visible";
+});
--- scripts/9199bcff.main.js
+++
@@ -1,4 +0,0 @@
-var f = new Firebase('https://jjperezaguinaga.firebaseio.com/photo_title');
-f.on('value', function(s) {
-  document.getElementById('image').src = s.val();
-});
